f671e3b9a42964b036634ff6cdc49809e7a0245c
Danuta <Danuta@DANUTA-PC>
2021-10-27 00:56:06 +0300

    Add project files.

---
+++ Module5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Module5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string CheckPhoneNumber = "+35 392-022-194";
+            //string CheckPhoneNumber = "+958 28492-503";
+
+            string pattern = @"^(\+\d{2}\s\d{3}-\d{3}-\d{3})";
+
+            var rgx = new Regex(pattern);
+
+            var match = rgx.Match(CheckPhoneNumber);
+
+            if (rgx.IsMatch(CheckPhoneNumber))
+            {
+                Console.WriteLine($"The phone number(\"{match.Value}\") --> true");
+            }
+            else
+            {
+                Console.WriteLine($"The phone number(\"{CheckPhoneNumber}\") --> false");
+            }
+
+            Console.ReadLine();            
+        }
+    }
+}
